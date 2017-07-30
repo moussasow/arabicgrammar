e718f46c0fbd7d3148ad0004e9807d922047b782
Sow Moussa <sow@sow.local>
2017-07-30 22:27:25 +0900

    add table view for lessons contents

--- ArabicGrammar/ViewController.m
+++ ArabicGrammar/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>{
+    NSMutableArray *mLessonArray;
+    
+}
+
+@property (weak, nonatomic) IBOutlet UITableView *mainTableView;
 
 @end
 
@@ -17,13 +22,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self createArabicGrammarLessons];
 }
 
+- (void) createArabicGrammarLessons {
+    mLessonArray = [NSMutableArray arrayWithArray:@[@"Harf Jarr", @"Harf Nasb", @"Adverbs of place", @"Adverbs of time", @"Numbers", @"Ordinals"]];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - UITableView DataSource Methods
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return mLessonArray.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString* cellIdentifier = @"cell";
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    //cell.imageView.image = [UIImage imageNamed:@"imagename"];
+    cell.textLabel.text = mLessonArray[indexPath.row];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%d", (int)indexPath.row + 1];
+    return cell;
+}
 
 @end
